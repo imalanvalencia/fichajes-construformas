4ce5bc1165689e60ab5a0f0f37b45fc7e249abc2
Alan Valencia <alanvalencia.contacto@gmail.com>
2026-06-06 23:24:12 +0200

    setup: init the backend project

---
+++ backend/src/test/java/es/constuformas/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.constuformas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
